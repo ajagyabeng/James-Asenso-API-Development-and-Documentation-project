f6ab0683a1d3e62de6b8375ec541fe14d8c5c86f
James Agyabeng <ajagyabeng@gmail.com>
2022-09-07 21:52:48 +0000

    Create test for delete questions

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -2,10 +2,13 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
-
+from dotenv import load_dotenv
 from flaskr import create_app
 from models import setup_db, Question, Category
 
+load_dotenv()
+
+DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -15,7 +18,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = f"{DATABASE_URI}/{self.database_name}"
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,7 +36,43 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
 
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+
+    def test_404_sent_request_beyond_valid_page(self):
+        res = self.client().get('/questions?page=100')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['message'], 'resource not found')
+        self.assertEqual(data['success'], False)
+
+    # -------------DELETE QUESTION--------------
+    def test_delete_questions(self):
+        res = self.client().delete('/questions/10')
+        data = json.loads(res.data)
+        question = Question.query.filter(Question.id == 10).first()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], 10)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+        self.assertEqual(question, None)
+
+    def test_404_for_question_does_not_exist(self):
+        res = self.client().delete('/questions/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['message'], 'resource not found')
+        self.assertEqual(data['success'], False)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
