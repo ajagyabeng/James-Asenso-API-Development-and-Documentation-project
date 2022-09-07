1d8f86bb9e3ef8b785ca49c617a248d0e935c0af
James Agyabeng <ajagyabeng@gmail.com>
2022-09-07 21:52:18 +0000

    Handle Lookup Error

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -9,6 +9,15 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+    """paginates the shows the number of items to display at a time"""
+    page = request.args.get('page', 1, type=int) 
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+    formatted_questions = [question.format() for question in selection]
+    current_questions = formatted_questions[start:end]
+    return current_questions
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
@@ -38,32 +47,23 @@ def create_app(test_config=None):
             'total_categories': len(Category.query.all())
         })
 
-    """
-    @TODO:
-    Create an endpoint to handle GET requests for questions,
-    including pagination (every 10 questions).
-    This endpoint should return a list of questions,
-    number of total questions, current category, categories.
-
-    TEST: At this point, when you start the application
-    you should see questions and categories generated,
-    ten questions per page and pagination at the bottom of the screen for three pages.
-    Clicking on the page numbers should update the questions.
-    """
     @app.route('/questions')
-    def get_questions():
-        page = request.args.get('page', 1, type=int)
-        response = Question.query.paginate(page=page, per_page=QUESTIONS_PER_PAGE)
-        questions = [question.format() for question in response.items]
+    def get_paginated_questions():
+        current_category = request.args.get('category', 1, type=int)
+        response = Question.query.all()
+        current_questions = paginate_questions(request, response)
         categories = [category.format() for category in Category.query.all()]
+
+        if len(current_questions) == 0:
+            abort(404)
         
         return jsonify({
             'success': True,
-            'questions': questions,
+            'questions': current_questions,
             'total_questions': len(Question.query.all()),
-            'categories': categories
+            'categories': categories,
+            'current_category': current_category
         })
-
 
     """
     @TODO:
@@ -72,6 +72,25 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
+        try:
+            question = Question.query.filter(Question.id == question_id).first()
+            if question is None:
+                abort(404)
+            question.delete()
+            response = Question.query.order_by(Question.id).all()
+            current_questions = paginate_questions(request, response)
+
+            return jsonify({
+                'success': True,
+                'deleted': question_id,
+                'questions': current_questions,
+                'total_questions': len(response)
+            })
+        except LookupError:
+            abort(422)
+
 
     """
     @TODO:
@@ -121,6 +140,26 @@ def create_app(test_config=None):
     Create error handlers for all expected errors
     including 404 and 422.
     """
+
+    # ----------ERROR HANDLERS-----------------
+
+    @app.errorhandler(404)
+    def not_found(error):
+        """handles 404 errors"""
+        return jsonify({
+            "success": False, 
+            "error": 404,
+            "message": "resource not found"
+            }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        """handles 422 errors"""
+        return jsonify({
+            "success": False, 
+            "error": 422,
+            "message": "Unprocessable"
+            }), 422
 
     return app
 
