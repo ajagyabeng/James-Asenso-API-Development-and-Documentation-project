72fa73e404a4623536db1bf02aede081ed83ea4b
James Agyabeng <ajagyabeng@gmail.com>
2022-09-09 10:00:26 +0000

    Modify database path

--- backend/models.py
+++ backend/models.py
@@ -2,9 +2,14 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URI = os.environ.get('DATABASE_URL')
 
 database_name = 'trivia'
-database_path = 'postgresql://{}/{}'.format('localhost:5432', database_name)
+database_path = f'{DATABASE_URI}/{database_name}'
 
 db = SQLAlchemy()
 
